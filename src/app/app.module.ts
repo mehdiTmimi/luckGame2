eae90b7e577063db1aebef3809cdaa73df950a9f
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2021-04-01 16:29:52 +0100

    template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './components/game/game.component';
+import { ScoreComponent } from './components/score/score.component';
+import { NumberComponent } from './components/game/number/number.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    ScoreComponent,
+    NumberComponent
   ],
   imports: [
     BrowserModule,
