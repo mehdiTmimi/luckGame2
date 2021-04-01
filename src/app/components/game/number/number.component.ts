f8cc875b6231a0d4314bed09dfa8efdb203cdb09
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2021-04-01 17:15:14 +0100

    examples

--- src/app/components/game/number/number.component.ts
+++ src/app/components/game/number/number.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NumberComponent implements OnInit {
 
-  constructor() { }
+  public n:number
+  public message:string;
+  public etat:boolean;
+  constructor() {
+    this.n= Math.floor(Math.random()*10);
+    this.message="bonjour"
+    this.etat=true;
+   }
 
   ngOnInit() {
+    setInterval(()=>{
+      this.n++;
+      this.etat=!this.etat;
+    },2000);
   }
-
+ 
+public getEtat():boolean{
+  return this.etat;
+}
+public getColor():string{
+  return this.etat?'green':'blue';
+}
 }
--- src/app/components/score/score.component.ts
+++ src/app/components/score/score.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScoreComponent implements OnInit {
 
+  public list:Array<number>;
+  public etatLoose:boolean;
   constructor() { }
 
   ngOnInit() {
+    this.etatLoose=true;
+    this.list=[5,3,5,2,6,22,7756,13,76,1346,657,46];
+    setInterval(()=>
+    {
+      this.list.pop()
+    },1000)
   }
 
 }
